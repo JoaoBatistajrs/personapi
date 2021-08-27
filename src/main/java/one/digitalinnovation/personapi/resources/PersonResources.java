4537aa809d35e61e5df7331a40f948c52e69143e
joaobatistajrs <joao.batista.jrs@gmail.comgit config --global user.email joao.batista.jrs@gmail.comgit config --global user.email joao.batista.jrs@gmail.com>
2021-08-27 09:44:58 -0300

    Aplica RestrController em person resource

--- src/main/java/one/digitalinnovation/personapi/resources/PersonResources.java
+++ src/main/java/one/digitalinnovation/personapi/resources/PersonResources.java
@@ -1,4 +1,16 @@
 package one.digitalinnovation.personapi.resources;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/person")
 public class PersonResources {
+
+    @GetMapping
+    public String getPerson(){
+        return "Api Teste";
+    }
 }
